6032f2f852b4b82219aca9b7a5dad3cce9095d4f
Sydney Brown <sydneykaylabrown@gmail.com>
2023-01-15 09:14:21 -0500

    Audio stuff

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+
+    [SerializeField] GameObject unmuteButton;
+    [SerializeField] GameObject muteButton;
+    [SerializeField] AudioSource audioSource;
+
+    private bool on = true;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetAudio();
+        audioSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void ToggleAudio()
     {
-        
+        on = !on;
+        if(on)
+        {
+        	audioSource.UnPause();
+
+        }
+        else
+        {
+        	audioSource.Pause();
+
+        }
+        SetAudio();
     }
+
+    void SetAudio()
+    {
+    	unmuteButton.SetActive(on);
+        muteButton.SetActive(on);
+
+    }
+
 }
